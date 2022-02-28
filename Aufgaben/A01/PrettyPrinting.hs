45130592e6d73297f759c643d02a64ed0cb7a88c
jowan <jowansulaiman@gmail.com>
2022-02-28 22:28:11 +0100

    Exercise 02.

--- Aufgaben/A01/PrettyPrinting.hs
+++ Aufgaben/A01/PrettyPrinting.hs
@@ -20,29 +20,29 @@ instance Pretty Term where
 -- | Instances for the predefined data types Term
    pretty  (Var (VarName x)) = x
    pretty  (Comb z [])       = z
-   pretty  (Comb z x)        = z ++ "(" ++ commaSep (getTerm x) ", " ++ ")"
+   pretty  (Comb z x)        = z ++ "(" ++ commaSep (dataTypesToString x) ", " ++ ")"
 
 instance Pretty Rule where
 -- | Instances for the predefined data types Rule
    pretty  (Rule (Var  (VarName x)) []) = x
    pretty  (Rule (Comb y []) []       ) =  y
-   pretty  (Rule (Comb y x ) []       ) =  y ++ "(" ++ commaSep (getTerm x) ", "        ++ ")."
-   pretty  (Rule (Comb y x ) z        ) = (y ++ "(" ++ commaSep (getTerm x) ", "       ++ ") :- ")    ++ commaSep (getTerm z) ", " ++ "."
+   pretty  (Rule (Comb y x ) []       ) =  y ++ "(" ++ commaSep (dataTypesToString x) ", "        ++ ")."
+   pretty  (Rule (Comb y x ) z        ) = (y ++ "(" ++ commaSep (dataTypesToString x) ", "       ++ ") :- ")    ++ commaSep (dataTypesToString z) ", " ++ "."
 
 instance Pretty Prog where
 -- | Instances for the predefined data types Prog
    pretty  (Prog []) = ""
-   pretty  (Prog x) = commaSep (getTerm x) "\n "
+   pretty  (Prog x) = commaSep (dataTypesToString x) "\n "
 
 instance Pretty Goal where
 -- | Instances for the predefined data types Goal
   pretty  (Goal []) = "?- ."
-  pretty  (Goal x ) = "?- " ++ commaSep (getTerm x) ", "  ++ ")."
+  pretty  (Goal x ) = "?- " ++ commaSep (dataTypesToString x) ", "  ++ ")."
 
-getTerm :: (Pretty a) => [a] -> [String]
--- | The function 'getTerm' converts a list of a into a list of string using pretty.
-getTerm [] = []
-getTerm (x:xs) = [pretty x]  ++ getTerm xs
+dataTypesToString :: (Pretty a) => [a] -> [String]
+-- | The function 'dataTypesToString' converts a list of a into a list of string using pretty.
+dataTypesToString [] = []
+dataTypesToString (x:xs) = [pretty x]  ++ dataTypesToString xs
 
 commaSep :: [String] -> String -> String
 -- | The function 'commaSep' Transform a list of strings into a comma separated string
