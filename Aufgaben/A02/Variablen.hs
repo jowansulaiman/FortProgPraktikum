4feaf122f085394412ba0156b9165e08f8bb4eef
Knell01 <rothenburgerkjell@gmail.com>
2022-03-03 12:02:00 +0100

    Minor changes on task 3
    - a few comment updates
    - fixed a minor error

--- Aufgaben/A02/Variablen.hs
+++ Aufgaben/A02/Variablen.hs
@@ -1,10 +1,11 @@
 {-|
 Module      : Variablen
-Description : determine existing variable names as well as create fresh variable names
+Description : determining existing variable names and creating an infinite amount of fresh ones
 Maintainer  : Jowan Sulaiman and Kjell Rothenburger
 the module contains a type class $Vars,
 which includes a method $'allVars' ::  a -> [VarName] This method returns all variables
-contained in a data type (without duplicates)..
+contained in a data type (without duplicates).
+and a function $freshVars which generates an infinite number of fresh variable names
 -}
 module Variablen where
 import Type
@@ -18,25 +19,25 @@ class (Show a) =>  (Vars a)  where
    allVars   x      = [VarName (show x)]
 
 instance Vars Term where
--- | Instances for the predefined data type Term
+-- | Instance for the predefined data type Term
    allVars  (Var (VarName x)) = [VarName x]
-   allVars  (Comb _ []  )     = [] --[VarName f]
-   allVars  (Comb _ (xs))     = nub (concat (map allVars xs)) --concat (map allVars xs) --concat (map allVars xs)
+   allVars  (Comb _ []  )     = []
+   allVars  (Comb _ (xs))     = nub (concat (map allVars xs))
 
 instance Vars Rule where
--- | Instances for the predefined data type Rule
-   allVars  (Rule t [])       = allVars t
-   allVars  (Rule t s)        = allVars  t  ++ nub (concat (map allVars s))
+-- | Instance for the predefined data type Rule
+   allVars  (Rule t []) = allVars t
+   allVars  (Rule t ts) = nub $ allVars t ++ concat (map allVars ts)
 
 instance Vars Prog where
--- | Instances for the predefined data type Prog
-   allVars  (Prog [])         = []
-   allVars  (Prog x)          = nub (concat (map allVars x))
+-- | Instance for the predefined data type Prog
+   allVars  (Prog [])    = []
+   allVars  (Prog rules) = nub (concat (map allVars rules))
 
 instance Vars Goal where
--- | Instances for the predefined data type Goal
-  allVars (Goal [])           = []
-  allVars (Goal x )           = nub (concat (map allVars x))
+-- | Instance for the predefined data type Goal
+  allVars (Goal [])  = []
+  allVars (Goal ts ) = nub (concat (map allVars ts))
 
 -- Returns an infinite List of Varnames as specified in task 3.2
 freshVars :: [VarName]
