cc22d4555b983e90848460a0148b424b666f373a
Knell01 <rothenburgerkjell@gmail.com>
2022-03-07 18:35:38 +0100

    Completed Umbenennung (Task 6)

--- Aufgaben/A01/Umbenennung.hs
+++ Aufgaben/A01/Umbenennung.hs
@@ -12,30 +12,54 @@ module Umbenennung
  where
 
 import Type
-import Data.List (nub, sort)
+import Data.List (nub)
 import PrettyPrinting
 import Test.QuickCheck
 import Variablen
+import Substitutionen
 
--- Resolutionsprinzip
-{-
-data Term = Var VarName | Comb CombName [Term]
-data Rule = Rule Term [Term]
--}
+
 rename :: [VarName] -> Rule -> Rule
--- | the function $'rename', which renames the variables of a rule.
-rename [] rule                   = rule
-rename [v]    (Rule (Var v1) []) = (Rule (Var v) [])
--- rename (x:xs) (Rule t []) = (Rule v [])
-
-
-intersect :: [VarName] -> [VarName] -> [VarName]
--- | intersection of two lists of VarName
-intersect l r = nub (helper l r)
+-- | Renames the rule using the freshVars function but ommitung the VarNames
+-- in the first argument
+rename l (Rule t ts) = let (y:ys) = renameTerms (l ++ (allVars t ++ (concatMap allVars ts)))
+                                     (allVars t ++ (concatMap allVars ts)) (t:ts) freshVars
+                         in Rule y ys
  where
-  helper [] _                 = []
-  helper (x:xs) l | elem x l  = x : helper xs l
-                  | otherwise = helper xs l
+  -- Renames the list of terms by iterating through (v:vs) using the names given
+  -- in nextNames but ommiting the VarNames given in f.
+  renameTerms :: [VarName] -> [VarName] -> [Term] -> [VarName] -> [Term]
+  --             forbidden     remaining   terms     nextNames
+  renameTerms f  []    ts ns   = renameAll_ f ts ns
+  renameTerms f (v:vs) ts (n:ns)
+   | n `elem` f                = renameTerms f (v:vs) ts ns
+   | v == VarName "_"          = renameTerms f vs ts (n:ns)
+   | otherwise                 = renameTerms f vs (map (apply (single v (Var n))) ts) ns
+
+  renameAll_ :: [VarName] -> [Term] -> [VarName] -> [Term]
+  -- | Renames every Variable "_"
+  renameAll_ f ts (n:ns)
+   | n `elem` f = renameAll_ f ts ns
+   | otherwise  = let new = rename_ ts n
+                    in if new == ts then ts else renameAll_ f new ns
+
+  rename_ :: [Term] -> VarName -> [Term]
+  -- | renames the first occurence of VarName "_" to the given VarName
+  rename_ ((Var vn):ts) v
+   | vn == VarName "_" = (Var v):ts
+   | otherwise         = (Var vn):(rename_ ts v)
+  rename_ ((Comb cn xs):ts) v
+   | (VarName "_") `elem` (allVars (Comb cn xs)) = (Comb cn (rename_ xs v)):ts
+   | otherwise                                   = (Comb cn xs):(rename_ ts v)
+  rename_ [] _ = []
+
+intersect :: Eq a => [a] -> [a] -> [a]
+intersect xs ys = helper (nub xs) ys
+ where
+  helper [] _ = []
+  helper (x:xs) ys
+   | x `elem` ys = x:(intersect xs ys)
+   | otherwise   = intersect xs ys
 
 --------------------------------------------{QuickCheck properties}-----------------------------------------------------
 
@@ -54,7 +78,7 @@ prop_4 xs r = (VarName "_") `notElem` allVars(r) ==>  length(allVars(rename xs r
 prop_5 :: [VarName] -> Rule -> Bool
 prop_5 xs r =  length(allVars(rename xs r )) >= length (allVars(r))
 
--- | return True, if it was successful.
+-- | return True if all tests are successful.
 return []
 -- | check all properties.
 checkProperties :: IO Bool
