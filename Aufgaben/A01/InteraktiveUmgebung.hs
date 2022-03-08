fa5e8726f489e0e8b6eba8dbd5b10b0e45388396
Knell01 <rothenburgerkjell@gmail.com>
2022-03-08 17:37:56 +0100

    Did most of the input cases
    Fehlend:
    - Programme einlesen (mittels parseFile)
    - SLD-Baum printen

--- Aufgaben/A01/InteraktiveUmgebung.hs
+++ Aufgaben/A01/InteraktiveUmgebung.hs
@@ -7,7 +7,7 @@ The module contains the following functions $'rename'
 The description of each function can be found below.
 -}
 module InteraktiveUmgebung
-  where
+where
 
 import Type
 import Data.List (nub, sort)
@@ -19,23 +19,68 @@ import Umbenennung
 import Data.Maybe(isNothing)
 import System.IO
 import Control.Monad
+import SLD_Resolution
+import PrettyPrinting
+import Parser
 
 interaktive :: IO ()
 interaktive = do
   putStr header
+  interaktiveLoop Nothing bfs Nothing
 
-interaktiveLoop ::  IO ()
-interaktiveLoop  = do      putStr header
-                           optionQuestion ""
+interaktiveLoop ::  Maybe Prog -> Strategy -> Maybe String -> IO ()
+interaktiveLoop p strat file = do
+ putStr "?- "
+ s <- getLine
+ processInput s p strat file
 
-optionQuestion :: String -> IO ()
-optionQuestion question = do
-  putStr (question ++ "?- ")
-  c <- getLine
-  case c of
-    ":h" -> putStrLn  helpHeader       >> optionQuestion question
-    ":q" -> return ()
-    _   -> putStrLn "Invalid input!" >> optionQuestion question
+processInput :: String -> Maybe Prog -> Strategy -> Maybe String -> IO ()
+processInput s p strat file
+  | s == ":h"                 = putStrLn helpHeader >> (interaktiveLoop p strat file)
+  | s == ":p"                 =  case p of
+                                  Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
+                                  (Just prog) -> putStrLn "Loaded program:" >> putStrLn (pretty prog) >> (interaktiveLoop p strat file)
+  | s == ":s dfs"             = putStrLn "Strategy set to depth-first search." >> interaktiveLoop p dfs file
+  | s == ":s bfs"             = putStrLn "Strategy set to breadth-first search." >> interaktiveLoop p bfs file
+  | s == ":q"                 = putStrLn "Successful termination."
+  | validGoal s = case p of
+                   Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
+                   (Just prog) -> putStrLn (show (sld prog (parseGoal s))) >> (interaktiveLoop p strat file)
+  -- | s == ":r"                 = case file of
+  --                                Nothing -> putStrLn "There is no last loaded file" >> interaktiveLoop p strat file
+  --                                Just f  -> readFileSave p strat f
+  -- | s == ":l " ++ fileName    = readFileSave p strat fileName
+  -- | s == ":t " ++ goal        = ...
+  | otherwise                    = putStrLn "Could not read the input. Please try again." >> interaktiveLoop p strat file
+
+
+parseGoal :: String -> Goal
+parseGoal s = case (parse s) of
+               Right g -> g
+               Left _  -> error "Unexpected error"
+
+
+validGoal :: String -> Bool
+validGoal s = case (parse s) :: Either String Goal of
+              Left _  -> False
+              Right _ -> True
+
+-- UNVOLLSTÄNDIG
+-- readFileSave :: Maybe Prog -> Strategy -> String
+-- readFileSave p strat s = parseFile ...
+
+-- optionQuestion :: String -> IO ()
+-- optionQuestion question = do
+--  putStr (question ++ "?- ")
+--  c <- getLine
+--  case c of
+--    ":h" -> putStrLn  helpHeader       >> optionQuestion question
+--    ":q" -> return ()
+--    _   -> putStrLn "Invalid input!" >> optionQuestion question
+
+-- unMaybe :: Maybe a -> a
+-- unMaybe (Just v)  = v
+-- unMaybe Nothing = error "Can't transform Nothing"
 
 
 helpHeader :: String
