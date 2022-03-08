7ffe91b9c54ecd30b82bff141a9dadc04994c0dd
Knell01 <rothenburgerkjell@gmail.com>
2022-03-08 20:45:04 +0100

    Completed Task 8
    (but still needs to be commented and thoroughly tested (Task 9))

--- Aufgaben/A01/InteraktiveUmgebung.hs
+++ Aufgaben/A01/InteraktiveUmgebung.hs
@@ -25,86 +25,75 @@ import Parser
 
 interaktive :: IO ()
 interaktive = do
-  putStr header
-  interaktiveLoop Nothing bfs Nothing
+ putStr header
+ interaktiveLoop Nothing bfs Nothing
 
 interaktiveLoop ::  Maybe Prog -> Strategy -> Maybe String -> IO ()
 interaktiveLoop p strat file = do
- putStr "?- "
- s <- getLine
- processInput s p strat file
+  putStr "?- "
+  s <- getLine
+  processInput s p strat file
 
 processInput :: String -> Maybe Prog -> Strategy -> Maybe String -> IO ()
 processInput s p strat file
-  | s == ":h"                 = putStrLn helpHeader >> (interaktiveLoop p strat file)
-  | s == ":p"                 =  case p of
-                                  Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
-                                  (Just prog) -> putStrLn "Loaded program:" >> putStrLn (pretty prog) >> (interaktiveLoop p strat file)
-  | s == ":s dfs"             = putStrLn "Strategy set to depth-first search." >> interaktiveLoop p dfs file
-  | s == ":s bfs"             = putStrLn "Strategy set to breadth-first search." >> interaktiveLoop p bfs file
-  | s == ":q"                 = putStrLn "Successful termination."
-  | validGoal s = case p of
-                   Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
-                   (Just prog) -> putStrLn (show (sld prog (parseGoal s))) >> (interaktiveLoop p strat file)
-  -- | s == ":r"                 = case file of
-  --                                Nothing -> putStrLn "There is no last loaded file" >> interaktiveLoop p strat file
-  --                                Just f  -> readFileSave p strat f
-  -- | s == ":l " ++ fileName    = readFileSave p strat fileName
-  | take 3 s == ":t "            = if validGoal (drop 3 s)
-                                    then case p of
-                                          Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
-                                          (Just prog) -> putStrLn (show (sld prog (parseGoal (drop 3 s)))) >> (interaktiveLoop p strat file)
-                                    else putStrLn "The goal is invalid." >> interaktiveLoop p strat file
-  | otherwise                    = putStrLn "Could not read the input. Please try again." >> interaktiveLoop p strat file
+ | s == ":h"                 = putStrLn helpHeader >> (interaktiveLoop p strat file)
+ | s == ":p"                 =  case p of
+                                 Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
+                                 (Just prog) -> putStrLn "Loaded program:" >> putStrLn (pretty prog) >> (interaktiveLoop p strat file)
+ | s == ":s dfs"             = putStrLn "Strategy set to depth-first search." >> interaktiveLoop p dfs file
+ | s == ":s bfs"             = putStrLn "Strategy set to breadth-first search." >> interaktiveLoop p bfs file
+ | s == ":q"                 = putStrLn "Successful termination."
+ | validGoal s               = case p of
+                                 Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
+                                 (Just prog) -> putStrLn (show (solveWith prog (parseGoal s) strat)) >> (interaktiveLoop p strat file)
+ | s == ":r"                 = case file of
+                                Nothing -> putStrLn "There is no last loaded file." >> interaktiveLoop p strat file
+                                Just f  -> readFileSave p strat f
+ | take 3 s == ":l "         = readFileSave p strat (drop 3 s)
+ | take 3 s == ":t "         = if validGoal (drop 3 s)
+                                then case p of
+                                       Nothing     -> putStrLn "No program loaded." >> (interaktiveLoop p strat file)
+                                       (Just prog) -> putStrLn (show (sld prog (parseGoal (drop 3 s)))) >> (interaktiveLoop p strat file)
+                                else putStrLn "The goal is invalid." >> interaktiveLoop p strat file
+ | otherwise                 = putStrLn "Could not read the input. Please try again." >> interaktiveLoop p strat file
 
 
 parseGoal :: String -> Goal
 parseGoal s = case (parse s) of
-               Right g -> g
-               Left _  -> error "Unexpected error"
+                Right g -> g
+                Left _  -> error "Unexpected error"
 
 
 validGoal :: String -> Bool
 validGoal s = case (parse s) :: Either String Goal of
-              Left _  -> False
-              Right _ -> True
+               Left _  -> False
+               Right _ -> True
 
--- UNVOLLSTÄNDIG
--- readFileSave :: Maybe Prog -> Strategy -> String
--- readFileSave p strat s = parseFile ...
-
--- optionQuestion :: String -> IO ()
--- optionQuestion question = do
---  putStr (question ++ "?- ")
---  c <- getLine
---  case c of
---    ":h" -> putStrLn  helpHeader       >> optionQuestion question
---    ":q" -> return ()
---    _   -> putStrLn "Invalid input!" >> optionQuestion question
-
--- unMaybe :: Maybe a -> a
--- unMaybe (Just v)  = v
--- unMaybe Nothing = error "Can't transform Nothing"
-
+readFileSave :: Maybe Prog -> Strategy -> String -> IO ()
+readFileSave p strat file = do
+  e <- (parseFile file) :: IO (Either String Prog)
+  (case e of
+    Left s            -> putStrLn "The file could not be found." >> interaktiveLoop p strat (Just file)
+    Right prog        -> putStrLn "Loaded." >> interaktiveLoop (Just prog) strat (Just file))
 
 helpHeader :: String
 helpHeader = unlines
-  [
-    " <goal>      Solves/proves the specified goal.   "
-  , " :h          Shows this help message.            "
-  , " :l <file>   Loads the specified file.           "
-  , " :p          Prints the currently loaded program."
-  , " :q          Exits the interactive environment.  "
-  , " :r          Reloads the last loaded file.       "
-  , " :s <strat>  Sets the specified search strategy  "
-  , "             where <strat> is one of 'dfs', 'bfs', or 'iddfs'."
-  , " :t <goal>   Prints the SLD tree for the specified goal.  "
-  ]
+ [
+   " <goal>      Solves/proves the specified goal.   "
+ , " :h          Shows this help message.            "
+ , " :l <file>   Loads the specified file.           "
+ , " :p          Prints the currently loaded program."
+ , " :q          Exits the interactive environment.  "
+ , " :r          Reloads the last loaded file.       "
+ , " :s <strat>  Sets the specified search strategy  "
+ , "             where <strat> is one of 'dfs', 'bfs', or 'iddfs'."
+ , " :t <goal>   Prints the SLD tree for the specified goal.  "
+ ]
 
 header :: String
 header = unlines
-  [
-    " Welcome!                               "
-  , " Type :h   for help.                    "
-  , "------------------------------------------"
-  ]
+ [
+   " Welcome!                               "
+ , " Type :h   for help.                    "
+ , "------------------------------------------"
+ ]
